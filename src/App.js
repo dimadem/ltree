1ba19b5f50dd8c11a11f9b8a7a308a3d726f2304
Maria <60796940+MariaKudryashova@users.noreply.github.com>
2023-01-26 00:54:17 +0300

    add dif samples fractals

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import Sketch from "react-p5";
 // Daniel Shiffman
 // Code for: https://youtu.be/E1B4UoSQMFw
 
-// variables: A B
-// axiom: A
-// rules: (A → AB), (B → A)
+//TODO
+//1 Сделать, чтобы масштаб можно было или задавать, или центрировать, чтобы не слетала картинка в облака
+//2 Сделать разные цвета на разных итерациях
+//3 Добавить разбор кошелька в алгоритм
 
-let angle = 60; //(60 * Math.PI) / 180; // угол
-let axiom = "F++F++F"; // аксиома
+//4 Придумать соответствие char кошелька к правилу (какие char на какую отрисовку - ветки, листья, разветвления)
+//5 Разбить итерация ==> char и так свое разбиение на уровень
+//6 Углы по char выбор соответствия
+//7 рандомайз по углу в секторе 
+
+let angle = 0; //(60 * Math.PI) / 180; // угол
+let axiom = ""; // аксиома
 let sentence = ""; // первая точка построения
 let len = 100; // длинна сегмента
 let iteration = 3; // итерации
@@ -17,9 +23,10 @@ let strw = 5; // толщина линии
 let strc = "gray"; // цвет линии
 let rules = []; // массив правил
 rules[0] = {
-  a: "F",
-  b: "F-F++F-F",
+  a: "",
+  b: "",
 };
+let mod = 0.03; //модификатор масштаба
 
 export default class App extends Component {
   setup = (p5, canvasParentRef) => {
@@ -54,7 +61,24 @@ export default class App extends Component {
         if (current === "F") {
           p5.line(0, 0, 0, -len);
           p5.translate(0, -len);
-        } else if (current === "+") {
+        } 
+        if (current === "L") {
+          p5.line(0, 0, 0, -len);
+          p5.translate(0, -len);
+        } 
+        if (current === "R") {
+          p5.line(0, 0, 0, -len);
+          p5.translate(0, -len);
+        } 
+        if (current === "A") {
+          p5.line(0, 0, 0, -len);
+          p5.translate(0, -len);
+        } 
+        if (current === "B") {
+          p5.line(0, 0, 0, -len);
+          p5.translate(0, -len);
+        } 
+        else if (current === "+") {
           p5.rotate(angle);
         } else if (current === "-") {
           p5.rotate(-angle);
@@ -67,45 +91,97 @@ export default class App extends Component {
     };
 
     // generate sentence for tree
-
     const generate = () => {
-      len *= 0.1; // модификатор на длинну сегмента на рендер
 
-      //0 итерация "F++F++F";
-      //1 итерация 'F-F++F-F++F-F++F-F++F-F++F-F'
-      //2 итерация 'F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F'
-      //F-F++F-F
+      /*//снежинка
+      angle = 60; 
+      axiom = "F++F++F"; 
+      rules[0] = {
+        a: "F",
+        b: "F-F++F-F",
+      };
+      iteration = 5;*/
 
-      let nextSentence = "";
-      let iteration = 9;
+      /*//Серпинский
+      angle = 60; 
+      axiom = "A"; 
+      rules[0] = {
+        a: "A",
+        b: "B-A-B"
+      };
+      rules[1] = {
+        a: "B",
+        b: "A+B+A"
+      };
+      mod = 0.025;
+      iteration = 8;*/
+
+
+     /* //дракон
+      angle = 90; 
+      axiom = "FX"; 
+      rules[0] = {
+        a: "X",
+        b: "X+YF"
+      };
+      rules[1] = {
+        a: "Y",
+        b: "FX-Y"
+      };
+      mod = 0.03;
+      iteration = 14;
+      */
+
+      //побеги
+      angle = 50; 
+      axiom = "X"; 
+      rules[0] = {
+        a: "F",
+        b: "FF"
+      };
+      rules[1] = {
+        a: "X",
+        b: "F[+X]F[-X]+X"
+      };
+      
+      mod = 0.03;
+      iteration = 7;
+
+      len *= mod; // модификатор на длинну сегмента на рендер
+
+      let nextSentence = "";      
       let temp = axiom; //что перебираем
       let temp_sent = ""; //темп формулы дерева
 
       for (let k = 0; k < iteration; k++) {
-        for (let i = 0; i < temp.length; i++) {
-          let ch = temp.charAt(i);
-          if (ch === "F") {
-            //found = true;
-            temp_sent += "F-F++F-F"; //F-F++F-F
-          } else {
-            temp_sent += ch;
-          }
-        }
+            temp_sent = "";
+            for (let i = 0; i < temp.length; i++) {
+                let ch = temp.charAt(i);
+                var checked = false;
+                //если ни одно из правил не подошло
+                for (let j = 0; j < rules.length; j++) {
+                    if (ch === rules[j].a) {
+                      temp_sent += rules[j].b; 
+                      checked = true;
+                      break;
+                    } 
+                }
+                //переносим символ из аксиомы
+                if (checked == false) {
+                    temp_sent += ch;
+                }
+            }
+        //перекладываем
         temp = temp_sent;
 
-        p5.createP("iteration:" + k + " temp:" + temp);
+        //p5.createP("iteration:" + k + " temp:" + temp);
       }
+      //готовая формула дерева
       sentence = temp;
 
-      //turtle();
+      turtle();
     };
-    //iterations
-    // noprotect
-    //for (let i = 0; i < iteration; i++) {
-    //  generate();
-    //}
-    generate();
-    turtle();
+    generate();    
   };
 
   render() {
