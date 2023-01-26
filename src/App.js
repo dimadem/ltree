33d63d1a2303cd58129771ee9e836e47ebcfb779
Dima <iamdimadem@gmail.com>
2023-01-26 14:17:02 +0000

    Обновление формулы отрисовки Л-Системы

--- src/App.js
+++ src/App.js
@@ -12,73 +12,87 @@ import Sketch from "react-p5";
 //4 Придумать соответствие char кошелька к правилу (какие char на какую отрисовку - ветки, листья, разветвления)
 //5 Разбить итерация ==> char и так свое разбиение на уровень
 //6 Углы по char выбор соответствия
-//7 рандомайз по углу в секторе 
+//7 рандомайз по углу в секторе
 
+// 64 цифры в кошельке
+// 1 цвет фона
+// 1 толщина линии
+// набор символов для правил генеративного паттерна фона
+// набор символо для правил генеративного дерева
+
+// settings default
 let angle = 0; //(60 * Math.PI) / 180; // угол
 let axiom = ""; // аксиома
 let sentence = ""; // первая точка построения
+let mod = 0.03; //модификатор масштаба
 let len = 100; // длинна сегмента
 let iteration = 3; // итерации
-let strw = 5; // толщина линии
-let strc = "gray"; // цвет линии
+let strWeight = 1; // толщина линии
+let scl = 1; // масштаб
 let rules = []; // массив правил
+let resRect = 350; // размер Canvas
+let posX = 0; // позиция по X
+let posY = 0; // позиция по Y
+let bgColor; // цвет фона
+// let strColor; // цвет линии
+
+// правила по умолчанию
 rules[0] = {
   a: "",
   b: "",
 };
-let mod = 0.03; //модификатор масштаба
 
 export default class App extends Component {
   setup = (p5, canvasParentRef) => {
-    p5.createCanvas(window.innerWidth, window.innerHeight).parent(
-      canvasParentRef
-    );
-    p5.frameRate(this.fr);
+    p5.createCanvas(resRect, resRect).parent(canvasParentRef);
     p5.angleMode(p5.DEGREES);
-    p5.background(200);
-    p5.createCanvas(window.innerWidth, window.innerHeight - 200).parent(
-      canvasParentRef
-    );
-
-    // AXIOM on SCREEN
-    //p5.createP(axiom);
-
+    // p5.background(200);
     p5.noLoop();
-
     // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
   };
 
   draw = (p5) => {
+    // отриовка
     const turtle = () => {
-      p5.background(200);
+      p5.background(bgColor);
       p5.resetMatrix();
-      p5.translate(p5.width / 2, p5.height / 1.3);
-      p5.stroke(strc);
-      p5.strokeWeight(strw);
-
+      //выравнивание обьекта
+      p5.translate(resRect - posX, resRect - posY);
+      p5.scale(scl);
+      ///////////////////////////
+      p5.strokeWeight(strWeight);
       for (let i = 0; i < sentence.length; i++) {
         let current = sentence.charAt(i);
         if (current === "F") {
+          //random color
+          p5.stroke(p5.random(0, 250), p5.random(0, 250), p5.random(0, 250));
           p5.line(0, 0, 0, -len);
           p5.translate(0, -len);
-        } 
+        }
         if (current === "L") {
+          //random color
+          p5.stroke(p5.random(0, 250), p5.random(0, 250), p5.random(0, 250));
           p5.line(0, 0, 0, -len);
           p5.translate(0, -len);
-        } 
+        }
         if (current === "R") {
+          //random color
+          p5.stroke(p5.random(0, 250), p5.random(0, 250), p5.random(0, 250));
           p5.line(0, 0, 0, -len);
           p5.translate(0, -len);
-        } 
+        }
         if (current === "A") {
+          //random color
+          p5.stroke(p5.random(0, 250), p5.random(0, 250), p5.random(0, 250));
           p5.line(0, 0, 0, -len);
           p5.translate(0, -len);
-        } 
+        }
         if (current === "B") {
+          //random color
+          p5.stroke(p5.random(0, 250), p5.random(0, 250), p5.random(0, 250));
           p5.line(0, 0, 0, -len);
           p5.translate(0, -len);
-        } 
-        else if (current === "+") {
+        } else if (current === "+") {
           p5.rotate(angle);
         } else if (current === "-") {
           p5.rotate(-angle);
@@ -92,85 +106,97 @@ export default class App extends Component {
 
     // generate sentence for tree
     const generate = () => {
+      //снежинка
+      // angle = 60;
+      // axiom = "F++F++F";
+      // rules[0] = {
+      //   a: "F",
+      //   b: "F-F++F-F",
+      // };
+      // mod = 0.01;
+      // iteration = 5;
 
-      /*//снежинка
-      angle = 60; 
-      axiom = "F++F++F"; 
-      rules[0] = {
-        a: "F",
-        b: "F-F++F-F",
-      };
-      iteration = 5;*/
+      // //Серпинский
+      // angle = 60;
+      // axiom = "A";
+      // rules[0] = {
+      //   a: "A",
+      //   b: "B-A-B",
+      // };
+      // rules[1] = {
+      //   a: "B",
+      //   b: "A+B+A",
+      // };
+      // // settings
+      // mod = 0.02;
+      // iteration = 10;
+      // strWeight = 1;
+      // scl = 2;
+      // posX = 350;
+      // posY = -40;
 
-      /*//Серпинский
-      angle = 60; 
-      axiom = "A"; 
-      rules[0] = {
-        a: "A",
-        b: "B-A-B"
-      };
-      rules[1] = {
-        a: "B",
-        b: "A+B+A"
-      };
-      mod = 0.025;
-      iteration = 8;*/
-
-
-     /* //дракон
-      angle = 90; 
-      axiom = "FX"; 
-      rules[0] = {
-        a: "X",
-        b: "X+YF"
-      };
-      rules[1] = {
-        a: "Y",
-        b: "FX-Y"
-      };
-      mod = 0.03;
-      iteration = 14;
-      */
+      //дракон
+      // angle = 90;
+      // axiom = "FX";
+      // rules[0] = {
+      //   a: "X",
+      //   b: "X+YF",
+      // };
+      // rules[1] = {
+      //   a: "Y",
+      //   b: "FX-Y",
+      // };
+      // //settings
+      // mod = 0.1;
+      // iteration = 8;
+      // scl = 1;
+      // posX = 150;
+      // posY = 120;
+      // strWeight = 2;
 
       //побеги
-      angle = 50; 
-      axiom = "X"; 
+      angle = 50;
+      axiom = "X";
       rules[0] = {
         a: "F",
-        b: "FF"
+        b: "FF",
       };
       rules[1] = {
         a: "X",
-        b: "F[+X]F[-X]+X"
+        b: "F[+X]F[-X]+X",
       };
-      
-      mod = 0.03;
-      iteration = 7;
+      // settings
+      mod = 0.01;
+      iteration = 10;
+      scl = 0.15;
+      strWeight = 6;
+      posX = 180;
+      posY = 20;
+      bgColor = p5.random(200, 250);
 
       len *= mod; // модификатор на длинну сегмента на рендер
 
-      let nextSentence = "";      
       let temp = axiom; //что перебираем
       let temp_sent = ""; //темп формулы дерева
 
       for (let k = 0; k < iteration; k++) {
-            temp_sent = "";
-            for (let i = 0; i < temp.length; i++) {
-                let ch = temp.charAt(i);
-                var checked = false;
-                //если ни одно из правил не подошло
-                for (let j = 0; j < rules.length; j++) {
-                    if (ch === rules[j].a) {
-                      temp_sent += rules[j].b; 
-                      checked = true;
-                      break;
-                    } 
-                }
-                //переносим символ из аксиомы
-                if (checked == false) {
-                    temp_sent += ch;
-                }
+        temp_sent = "";
+        for (let i = 0; i < temp.length; i++) {
+          let ch = temp.charAt(i);
+          var checked = false;
+          //если ни одно из правил не подошло
+          for (let j = 0; j < rules.length; j++) {
+            if (ch === rules[j].a) {
+              temp_sent += rules[j].b;
+              checked = true;
+              break;
             }
+          }
+          //переносим символ из аксиомы
+          if (checked === false) {
+            temp_sent += ch;
+          }
+        }
         //перекладываем
         temp = temp_sent;
 
@@ -181,7 +207,7 @@ export default class App extends Component {
 
       turtle();
     };
-    generate();    
+    generate();
   };
 
   render() {
